ceb5e7f84c45bcdf4e210d5c7e8c5ab867326f28
Larisa Markova <lovemirror@rambler.ru>
2024-10-31 15:45:50 +0300

    Fix Badge basic example

--- src/components/Badge/BadgeBasic.tsx
+++ src/components/Badge/BadgeBasic.tsx
@@ -1,28 +1,11 @@
 import styled from 'styled-components';
-import { Badge, typography } from '@admiral-ds/react-ui';
-import type { BadgeProps } from '@admiral-ds/react-ui';
+import { Badge } from '@admiral-ds/react-ui';
 
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   padding: 50px;
-  & > * {
-    padding: 8px 0 8px 16px;
-  }
-`;
-
-const String = styled.div<{ appearance?: BadgeProps['appearance'] }>`
-  display: flex;
-  align-items: center;
-  ${({ appearance, theme }) =>
-    appearance &&
-    appearance.indexOf('white') > -1 &&
-    `background: var(--admiral-color-Neutral_Neutral05, ${theme.color['Neutral/Neutral 05']});`}
-  & > * {
-    margin-right: 16px;
-  }
-  ${typography['Body/Body 2 Long']}
-  color: var(--admiral-color-Neutral_Neutral90, ${(p) => p.theme.color['Neutral/Neutral 90']});
 `;
 
 export const BadgeBasic = () => {
