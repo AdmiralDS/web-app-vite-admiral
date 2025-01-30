6141b2ed73d37e392a3fe47c04c3dc622d5bbadc
EskeeT <angel.show8950@gmail.com>
2025-01-30 12:25:29 +0300

    add DateInput and DateField example

---
+++ src/routes/components/dateInput/variants.tsx
@@ -0,0 +1,51 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { ExampleSection } from '../../-helpers/examples';
+import { DateInput } from '@admiral-ds/react-ui';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const DateInputStyled = styled(DateInput)`
+  max-width: 300px;
+`;
+
+export const Template = () => {
+  const [localValue, setValue] = useState('');
+  const [localValue2, setValue2] = useState('');
+  const [localValue3, setValue3] = useState('');
+
+  return (
+    <>
+      <ExampleSection text="День">
+        <DateInputStyled
+          currentActiveView="DAY"
+          value={localValue}
+          onChange={(e) => setValue(e.currentTarget.value)}
+          dropContainerClassName="dropContainerClass"
+        />
+      </ExampleSection>
+      <ExampleSection text="Месяц">
+        <DateInputStyled
+          currentActiveView="MONTH"
+          value={localValue2}
+          onChange={(e) => setValue2(e.currentTarget.value)}
+          dropContainerClassName="dropContainerClass"
+        />
+      </ExampleSection>
+      <ExampleSection text="Год">
+        <DateInputStyled
+          currentActiveView="YEAR"
+          value={localValue3}
+          onChange={(e) => setValue3(e.currentTarget.value)}
+          dropContainerClassName="dropContainerClass"
+        />
+      </ExampleSection>
+    </>
+  );
+};
+
+export const Route = createFileRoute('/components/dateInput/variants')({
+  component: () => <Template />,
+  staticData: {
+    title: 'DateInput. Варианты отображения дат',
+  },
+});
