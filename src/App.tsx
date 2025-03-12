b7c57fc3dfe813a8c4d6a841cce9cc7765732a77
Andrey Litvinov <drune.email@gmail.com>
2025-03-12 19:08:57 +0300

    add date picker example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import './App.css';
-
+import { lightThemeClassName } from '@admiral-ds/web';
+import { DatePicker } from '@admiral-ds/date-picker';
+import { maskitoDateOptionsGenerator } from '@maskito/kit';
+import { useMaskito } from '@maskito/react';
 import { TableLoadOnScroll } from './table/example1';
 import { TableLoadOnScrollSpinner } from './table/example2';
 import { TableLoadOnScrollSkeleton } from './table/example3';
@@ -20,6 +23,14 @@ import { CategoryBusOutline } from '@admiral-ds/icons';
 
 import { T, Link } from '@admiral-ds/react-ui';
 
+const dateOptions = maskitoDateOptionsGenerator({ mode: 'dd/mm/yyyy' });
+
+const defaultInputProps = {
+  placeholder: 'Введите дату',
+  dataPlaceholder: 'дд.мм.гггг',
+  value: '11.',
+} satisfies React.ComponentProps<typeof DatePicker>['inputProps'];
+
 const Divider = styled.div`
   width: 10px;
   height: 12px;
@@ -27,9 +38,24 @@ const Divider = styled.div`
 
 function App() {
   const [count, setCount] = useState(0);
+  const [inputValue, setInputValue] = useState(defaultInputProps.value);
+  const maskedDateInputRef = useMaskito({ options: dateOptions });
+
+  const dateProps = {
+    inputProps: {
+      ...defaultInputProps,
+      value: inputValue,
+      onInput: (e) => setInputValue(e.currentTarget.value),
+      ref: maskedDateInputRef,
+    },
+  } satisfies React.ComponentProps<typeof DatePicker>;
+
+  useEffect(() => {
+    document.body.classList.add(...lightThemeClassName.split(' '));
+  }, []);
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           {/* импорт иконки через директорию assets. Доступ к этой директории есть и у index.html */}
@@ -47,6 +73,7 @@ function App() {
         <T font="Subtitle/Subtitle 1" as="p">
           Edit <code>src/App.tsx</code> and save to test HMR
         </T>
+        <DatePicker {...dateProps} />
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
       <Link appearance="primary" href="https://admiralds.github.io/react-ui" target="_blank" rel="noopener noreferrer">
@@ -64,7 +91,7 @@ function App() {
         <h3>Taблица со скелетонами и подгрузкой данных при скролле</h3>
         <TableLoadOnScrollSkeleton />
       </div>
-    </>
+    </div>
   );
 }
 
