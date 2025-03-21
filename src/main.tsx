10b3f1ce47788ba65abf2657f7c023e73dde1e18
Andrey Litvinov <drune.email@gmail.com>
2025-03-21 12:33:09 +0300

    revert to use react 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // React < 18
+// import ReactDOM from 'react-dom/client'; // React 18+
 import { ThemeProvider } from 'styled-components';
 import { LIGHT_THEME, FontsVTBGroup, DropdownProvider } from '@admiral-ds/react-ui';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+// React < 18
+ReactDOM.render(
+  React.createElement(() => (
     <ThemeProvider theme={LIGHT_THEME}>
       <DropdownProvider>
         <FontsVTBGroup />
         <App />
       </DropdownProvider>
     </ThemeProvider>
-  </React.StrictMode>,
+  )),
+  document.getElementById('root'),
 );
+
+// For React 18+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={LIGHT_THEME}>
+//       <DropdownProvider>
+//         <FontsVTBGroup />
+//         <App />
+//       </DropdownProvider>
+//     </ThemeProvider>
+//   </React.StrictMode>,
+// );
