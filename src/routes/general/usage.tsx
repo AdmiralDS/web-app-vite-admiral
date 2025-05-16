2fa1a998104ca04f20813f002c8fb79b6b53b9d2
Kutepova Ekaterina <eovetelina@gmail.com>
2025-05-16 15:13:40 +0400

    fix mistakes at installaion and usage pages

--- src/routes/general/usage.tsx
+++ src/routes/general/usage.tsx
@@ -4,7 +4,7 @@ import { useMemo } from 'react';
 import { Link } from '@admiral-ds/react-ui';
 
 import { CodeBlock } from '../-helpers/general';
-import { SectionDescription } from '../-helpers/examples';
+import { SectionDescription } from '../../examples/-helpers';
 
 const Separator = styled.div<{ height: number }>`
   height: ${(p) => p.height}px;
