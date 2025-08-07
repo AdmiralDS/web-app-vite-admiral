e98c7b6e14ffe188971082d0a1d182bd24de13fc
babaev_adil <babaev.adil8950@gmail.com>
2025-08-07 13:30:13 +0300

    feat: add baseExample tanstack table

---
+++ src/examples/tanstackTable/baseExample.tsx
@@ -0,0 +1,148 @@
+import * as React from 'react';
+
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import {
+  Body,
+  HeaderTr,
+  HeaderCellTh,
+  HeaderWrapper,
+  TableContainer,
+  BodyTr,
+  CellTd,
+  ThWrapper,
+  RowLine,
+} from './styled';
+import styled from 'styled-components';
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  status: string;
+  progress: number;
+};
+
+const defaultData: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+];
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  margin: 20px;
+  overflow: hidden;
+  background: var(--admiral-color-Neutral_Neutral00, ${(p) => p.theme.color['Neutral/Neutral 00']});
+`;
+
+const columnHelper = createColumnHelper<Person>();
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: 'lastName',
+    cell: (info) => <i>{info.getValue()}</i>,
+    header: () => <span>Last Name</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('age', {
+    header: () => 'Age',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('visits', {
+    header: () => <span>Visits</span>,
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('progress', {
+    header: 'Profile Progress',
+    footer: (info) => info.column.id,
+  }),
+];
+
+export const BaseExample = () => {
+  const [data, _setData] = React.useState(() => [...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <Wrapper>
+      <TableContainer>
+        <HeaderWrapper>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <HeaderTr $dimension="m" key={headerGroup.id}>
+              {headerGroup.headers.map((header, id) => (
+                <HeaderCellTh $dimension="m" key={header.id}>
+                  <ThWrapper $dimension="m">
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    {headerGroup.headers.length !== id + 1 && <RowLine />}
+                  </ThWrapper>
+                </HeaderCellTh>
+              ))}
+            </HeaderTr>
+          ))}
+        </HeaderWrapper>
+        <Body>
+          {table.getRowModel().rows.map((row) => (
+            <BodyTr>
+              {row.getVisibleCells().map((cell) => (
+                <CellTd $dimension="m" key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </CellTd>
+              ))}
+            </BodyTr>
+          ))}
+        </Body>
+        {/* <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <HeaderTr $dimension="m" key={footerGroup.id}>
+              {footerGroup.headers.map((header, id) => (
+                <HeaderCellTh $dimension="m" key={header.id}>
+                  <ThWrapper $dimension="m">
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.footer, header.getContext())}
+                    {footerGroup.headers.length !== id + 1 && <RowLine />}
+                  </ThWrapper>
+                </HeaderCellTh>
+              ))}
+            </HeaderTr>
+          ))}
+        </tfoot> */}
+      </TableContainer>
+    </Wrapper>
+  );
+};
---
+++ src/examples/tanstackTable/styled.tsx
@@ -0,0 +1,120 @@
+import { typography } from '@admiral-ds/react-ui';
+import styled, { css } from 'styled-components';
+
+export type Dimension = 'xl' | 'l' | 'm' | 's';
+
+export const borderStyle = css<{ $resizer?: boolean }>`
+  border-right: 1px solid transparent;
+  [data-borders='true'] & {
+    border-color: ${(p) =>
+      p.$resizer ? `var(--admiral-color-Neutral_Neutral20, ${p.theme.color['Neutral/Neutral 20']})` : 'transparent'};
+  }
+`;
+
+// padding-bottom меньше padding-top на 1px, т.к. 1px остается для border-bottom ячейки
+export const cellStyle = css<{ $dimension: Dimension }>`
+  padding: ${({ $dimension }) => {
+    switch ($dimension) {
+      case 's':
+        return '6px 12px 5px 12px';
+      case 'l':
+        return '12px 16px 11px 16px';
+      case 'xl':
+        return '16px 16px 15px 16px';
+      case 'm':
+      default:
+        return '10px 0px 9px 12px';
+    }
+  }};
+`;
+
+export const headerStyle = css<{ $dimension: Dimension }>`
+  color: var(--admiral-color-Neutral_Neutral90, ${(p) => p.theme.color['Neutral/Neutral 90']});
+  ${({ $dimension }) =>
+    $dimension === 'l' || $dimension === 'xl' ? typography['Subtitle/Subtitle 2'] : typography['Subtitle/Subtitle 3']}
+`;
+
+export const rowStyle = css<{ $dimension: Dimension }>`
+  color: var(--admiral-color-Neutral_Neutral90, ${(p) => p.theme.color['Neutral/Neutral 90']});
+  ${({ $dimension }) =>
+    $dimension === 'l' || $dimension === 'xl' ? typography['Body/Body 1 Short'] : typography['Body/Body 2 Short']}
+`;
+
+// устанавливаем  pointer-events: none для ResizerWrapper во время drag&drop столбцов, так как ResizerWrapper
+// располагается прямо между соседними столбцами, и это мешает правильно рассчитать то, над каким столбцом находится курсор
+export const TableContainer = styled.table`
+  border-collapse: collapse;
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  background: var(--admiral-color-Neutral_Neutral00, ${(p) => p.theme.color['Neutral/Neutral 00']});
+  overflow: auto;
+  /* 
+  &[data-borders='true'] {
+    border: 1px solid var(--admiral-color-Neutral_Neutral20, ${(p) => p.theme.color['Neutral/Neutral 20']});
+  } */
+`;
+
+export const HeaderWrapper = styled.thead`
+  box-sizing: border-box;
+  position: sticky;
+  top: 0;
+  z-index: 6;
+  background: var(--admiral-color-Neutral_Neutral00, ${(p) => p.theme.color['Neutral/Neutral 00']});
+`;
+
+export const HeaderTr = styled.tr<{
+  $dimension: Dimension;
+  $greyHeader?: boolean;
+}>`
+  box-sizing: border-box;
+  min-width: fit-content;
+  ${headerStyle}
+  background: ${(p) =>
+    p.$greyHeader
+      ? `var(--admiral-color-Neutral_Neutral05, ${p.theme.color['Neutral/Neutral 05']})`
+      : `var(--admiral-color-Neutral_Neutral00, ${p.theme.color['Neutral/Neutral 00']})`};
+`;
+
+export const HeaderCellTh = styled.th<{ $dimension: Dimension; $resizer?: boolean }>`
+  position: relative;
+  box-sizing: border-box;
+  ${borderStyle}
+  cursor: default;
+  &[data-draggable='true'] {
+    cursor: pointer;
+  }
+  text-align: start;
+  border-bottom: 1px solid var(--admiral-color-Neutral_Neutral20, ${(p) => p.theme.color['Neutral/Neutral 20']});
+`;
+
+export const Body = styled.tbody``;
+
+export const BodyTr = styled.tr`
+  position: relative;
+  background: var(--admiral-color-Neutral_Neutral00, ${(p) => p.theme.color['Neutral/Neutral 00']});
+`;
+
+export const CellTd = styled.td<{ $dimension: Dimension; $resizer?: boolean }>`
+  box-sizing: border-box;
+  ${cellStyle};
+  overflow: hidden;
+  ${borderStyle}
+  text-align: start;
+  border-bottom: 1px solid var(--admiral-color-Neutral_Neutral20, ${(p) => p.theme.color['Neutral/Neutral 20']});
+  ${rowStyle}
+`;
+
+export const ThWrapper = styled.div<{ $dimension: Dimension }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ${cellStyle};
+`;
+
+export const RowLine = styled.div`
+  margin-left: 12px;
+  width: 1px;
+  height: 16px;
+  background-color: var(--admiral-color-Neutral_Neutral20, ${(p) => p.theme.color['Neutral/Neutral 20']});
+`;
