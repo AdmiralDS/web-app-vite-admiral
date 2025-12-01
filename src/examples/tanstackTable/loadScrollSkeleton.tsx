3c46fde0b13057327399468637133aeca3854328
Kutepova Ekaterina <eovetelina@gmail.com>
2025-12-01 17:18:57 +0300

    refactor: remove some props, add useCallback, rewrite imports in skeleton example

--- src/examples/tanstackTable/loadScrollSkeleton.tsx
+++ src/examples/tanstackTable/loadScrollSkeleton.tsx
@@ -1,29 +1,27 @@
-import * as React from 'react';
-
 import { getCoreRowModel, useReactTable, type ColumnDef, type Row } from '@tanstack/react-table';
-import { CellText, defaultOptions, TanstackTable } from '#examples/-helpers/tanstackTable';
-import { ExampleSection } from '#examples/-helpers';
 import { PseudoText, skeletonAnimationMixin } from '@admiral-ds/react-ui';
 import styled from 'styled-components';
-import { useMemo } from 'react';
+import { useMemo, useState, useRef, useEffect, useCallback } from 'react';
+
+import { CellText, defaultOptions, TanstackTable } from '#examples/-helpers/tanstackTable';
+import { ExampleSection } from '#examples/-helpers';
 
 const ROW_AMOUNT = 100;
-
-type LastRowProps = {
-  containerRef: React.RefObject<HTMLElement>;
-  onVisible: () => void;
-  rowNode: React.ReactNode;
-  rowIndex?: number;
-};
 
 const SkeletonComponent = styled(PseudoText)`
   width: 100%;
   ${skeletonAnimationMixin}
 `;
 
-const LastRowWrapper = ({ containerRef, onVisible, rowNode }: LastRowProps) => {
-  const [visible, setVisible] = React.useState<boolean>(false);
-  const ref = React.useRef<HTMLDivElement>(null);
+type LastRowProps = {
+  container: HTMLElement | null;
+  onVisible: () => void;
+  rowNode: React.ReactNode;
+};
+
+const LastRowWrapper = ({ container, onVisible, rowNode }: LastRowProps) => {
+  const [visible, setVisible] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>(null);
 
   const handleIntersection = (entries: IntersectionObserverEntry[]) => {
     if (entries[0].isIntersecting && entries[0].intersectionRatio > 0 && !visible) {
@@ -36,18 +34,18 @@ const LastRowWrapper = ({ containerRef, onVisible, rowNode }: LastRowProps) => {
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     const observer = new IntersectionObserver(handleIntersection, {
-      root: containerRef?.current,
+      root: container,
       threshold: [0, 1.0],
     });
 
-    if (containerRef.current && ref.current) {
+    if (container && ref.current) {
       observer.observe(ref.current);
     }
 
     return () => observer.disconnect();
-  }, [visible, rowNode]);
+  }, [visible, container]);
 
   return <div ref={ref}>{rowNode}</div>;
 };
@@ -59,9 +57,9 @@ interface Props {
 }
 
 export const LoadScrollSkeletonExample = () => {
-  const [rowsAmount, setRowsAmount] = React.useState(10);
-  const [loading, setLoading] = React.useState(false);
-  const tableRef = React.useRef<HTMLDivElement>(null);
+  const [rowsAmount, setRowsAmount] = useState(10);
+  const [loading, setLoading] = useState(false);
+  const [tableContainer, setTableContainer] = useState<HTMLElement | null>(null);
 
   const columns: ColumnDef<Props>[] = useMemo(
     () => [
@@ -111,7 +109,7 @@ export const LoadScrollSkeletonExample = () => {
     [rowsAmount, loading],
   );
 
-  const data: Props[] = React.useMemo(() => {
+  const data: Props[] = useMemo(() => {
     const array = Array.from({ length: rowsAmount }, (_, k) => {
       return `${k + 1}0000`;
     }).map((item, index) => ({
@@ -122,7 +120,9 @@ export const LoadScrollSkeletonExample = () => {
     return array;
   }, [rowsAmount]);
 
-  const uploadNewRows = () => {
+  const handleRef = useCallback((node: HTMLElement | null) => setTableContainer(node), []);
+
+  const uploadNewRows = useCallback(() => {
     if (rowsAmount >= ROW_AMOUNT) return;
 
     setLoading(true);
@@ -132,21 +132,23 @@ export const LoadScrollSkeletonExample = () => {
     promise.then(() => {
       setLoading(false);
     });
-  };
+  }, [rowsAmount]);
 
-  const renderRowWrapper = (row: Row<Props>, index: number, rowNode: React.ReactNode) =>
-    index === rowsAmount - 1 && index !== ROW_AMOUNT - 1 ? (
-      <LastRowWrapper
-        key={`${row.id}`}
-        data-index={index}
-        containerRef={tableRef}
-        onVisible={uploadNewRows}
-        rowNode={rowNode}
-        rowIndex={index}
-      />
-    ) : (
-      rowNode
-    );
+  const renderRowWrapper = useCallback(
+    (row: Row<Props>, index: number, rowNode: React.ReactNode) =>
+      index === rowsAmount - 1 && index !== ROW_AMOUNT - 1 ? (
+        <LastRowWrapper
+          key={`${row.id}`}
+          data-index={index}
+          container={tableContainer}
+          onVisible={uploadNewRows}
+          rowNode={rowNode}
+        />
+      ) : (
+        rowNode
+      ),
+    [rowsAmount, tableContainer, uploadNewRows],
+  );
 
   const table = useReactTable({
     data,
@@ -163,7 +165,7 @@ export const LoadScrollSkeletonExample = () => {
         IntersectionObserver отслеживать момент, когда элемент-обёртка станет видим в пределах тела таблицы (т.е. момент
         доскролла до последней строки). Это событие будет являться триггером для загрузки новой порции строк."
       >
-        <TanstackTable table={table} style={{ height: '350px' }} renderRowWrapper={renderRowWrapper} ref={tableRef} />
+        <TanstackTable table={table} style={{ height: '350px' }} renderRowWrapper={renderRowWrapper} ref={handleRef} />
       </ExampleSection>
     </>
   );
