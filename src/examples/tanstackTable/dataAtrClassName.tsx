5bb0bf99b83a95e907ecd019e6d25865a8611e86
babaev_adil <babaev.adil8950@gmail.com>
2025-12-11 09:34:27 +0300

    fix: table dimension in dataAtrClassName example

--- src/examples/tanstackTable/dataAtrClassName.tsx
+++ src/examples/tanstackTable/dataAtrClassName.tsx
@@ -124,7 +124,7 @@ export const DataAtrClassNameExample = () => {
         </>
       }
     >
-      <TanstackTable table={tableDocs} dimension="s" showBorders />
+      <TanstackTable table={tableDocs} showBorders />
     </ExampleSection>
   );
 };
